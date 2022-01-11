24333c4011176a2cf0b20b4217121d2def9a22e8
ABRAR ALQARNI <abraralqarni@MacBook-Pro-ABRAR.local>
2022-01-11 16:54:56 +0300

    P-2022_01_11 (Profesionals) Sign up and login

---
+++ Part-time job /Part-time job/View/AddPostTableVC.swift
@@ -0,0 +1,76 @@
+//
+//  AddPostTableVC.swift
+//  Part-time job
+//
+//  Created by ABRAR ALQARNI on 22/05/1443 AH.
+//
+
+import UIKit
+
+class AddPostTableVC: UIViewController {
+  
+  //  MARK: - @IBOutlet
+  @IBOutlet weak var addPostTableView: UITableView!
+  
+
+  var dbb =  [" "]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+   
+    addPostTableView.dataSource = self
+    addPostTableView.delegate = self
+
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+     
+//      self.addPostTableView.reloadData()
+//
+//    }
+  }
+  
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "ShowDetail" {
+      let destanation = segue.destination as! UploadigJobDetailsVC
+      let selectedIndexPath = addPostTableView.indexPathForSelectedRow!
+//      destanation.spot = spots.spotArray[selectedIndexPath.row]
+      
+    }
+  }
+}
+
+// MARK: - Table view data source
+extension AddPostTableVC: UITableViewDelegate , UITableViewDataSource {
+  //Number Of Rows
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return dbb.count
+//
+  }
+  // Cell Data
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! AddPostTabelCell
+    
+   // cell.kindOfJobLabel.text = dbb[indexPath.row].name
+
+    return cell
+  }
+  
+  
+  //  tableView.dequeueReusableCell(withIdentifier: "JobCell") as! JobTableViewCell
+  //  cell.JobNameLabel.text = JobeTableArr[indexPath.row].jobName
+  //  cell.JobTimeLabel.text = JobeTableArr[indexPath.row].jobTime
+  //  cell.JobOfferLabel.text = JobeTableArr[indexPath.row].jobOffer
+  //  cell.JobImage.image = JobeTableArr[indexPath.row].image
+  //
+  
+  
+}
